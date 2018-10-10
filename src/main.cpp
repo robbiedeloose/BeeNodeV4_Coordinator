a6c74bb3bd314c772db3153229b0e220b1b85092
robbiedeloose <robbiedeloose@users.noreply.github.com>
2018-10-10 23:35:22 +0200

    changed HX711 library and scale function

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,7 @@
   #include <SPI.h>
 
 /******************************* BOARD FUNCTIONS ******************************/
-  #define Serial SerialUSB 
+  #define Serial SerialUSB
   ////////////////////////////////// SERIAL FLASH ////////////////////////////////
   #include <SerialFlash.h>
   const int flashChipSelect = flashChipCSPin;
@@ -77,11 +77,32 @@
   BH1750 lightMeter;
 
   ////////////////////////////////// SCALES ////////////////////////////////////////
-    #include "HX711-multi.h"
-    byte DOUTS[9] = {scaleData1, scaleData2, scaleData3, scaleData4, scaleData5, scaleData6, scaleData7, scaleData8, scaleData9};
-    #define CHANNEL_COUNT (int)(sizeof(DOUTS)/sizeof(byte))
-    long int results[CHANNEL_COUNT];
-    HX711MULTI scales(CHANNEL_COUNT, DOUTS, scaleClock);
+  #include "HX711.h"
+
+  #define SCALE_1_CLOCK 7  // wit-blauw
+  #define SCALE_1_DATA 8   // blauw
+  #define SCALE_2_CLOCK 9  // wit-groen
+  #define SCALE_2_DATA 10   // groen
+  #define SCALE_3_CLOCK 11  // wit-bruin
+  #define SCALE_3_DATA 12   // bruin
+
+  /*
+  #define SCALE_4_CLOCK 9  // geel
+  #define SCALE_4_DATA 8   // blauw
+  #define SCALE_5_CLOCK 11 // geel
+  #define SCALE_5_DATA 10  // blauw
+  #define SCALE_6_CLOCK 13 // geel
+  #define SCALE_6_DATA 12  // blauw
+  */
+
+  // DT, SCK  // parameter "gain" is ommited; the default value 128 is used by the library
+  HX711 scale1(SCALE_1_DATA, SCALE_1_CLOCK); 
+  HX711 scale2(SCALE_2_DATA, SCALE_2_CLOCK); 
+  HX711 scale3(SCALE_3_DATA, SCALE_3_CLOCK);
+  //HX711 scale4(SCALE_4_DATA, SCALE_4_CLOCK); 
+  //HX711 scale5(SCALE_5_DATA, SCALE_5_CLOCK); 
+  //HX711 scale6(SCALE_6_DATA, SCALE_6_CLOCK); 
+
 
 /******************************* STRUCTS **************************************/
   ///////////////////////////////////// STRUCTS & STRUCT ARAYS ///////////////////
@@ -90,7 +111,7 @@
     uint16_t baseHum;
     uint16_t baseLux;
     uint16_t baseBat;
-    long int weights[CHANNEL_COUNT];
+    long int weights[3];
   };
 
 /******************************* FUNCTION DECLARATIONS ************************/
@@ -107,7 +128,7 @@
   // Sensors
   void getCoordinatorData(LocalData_t *local);
   void getWeatherData(LocalData_t *local);
-  void getScaleData(LocalData_t *local, int nbOfReads = 10, int maxDeviation = 1000);
+  void getScaleData(LocalData_t *local);
   void showLocalData(LocalData_t *local);
   // Communications
   void gprsTest();
@@ -248,32 +269,26 @@ void getCoordinatorData(LocalData_t *local) {
 
 void getWeatherData(LocalData_t *local) {
   Serial.println(":: getWeatherData");
+  Serial.println("temp");
   local->baseTemp = myHumidity.readTemperature() * 100;
+  Serial.println("hum");
   local->baseHum = myHumidity.readHumidity() * 100;
+  Serial.println("light");
   local->baseLux = lightMeter.readLightLevel();
 }
 
-void getScaleData(LocalData_t *local, int nbOfReads, int maxDeviation) {
+void getScaleData(LocalData_t *local) {
   Serial.println(":: getScaleData");
-  long int weights[nbOfReads][CHANNEL_COUNT];
-  for (int read = 0; read < nbOfReads; read++) {
-    scales.read(weights[read]);
-    delay(500);
-  }
-  for (int channel = 0; channel < CHANNEL_COUNT; channel++) {
-    int sum = 0;
-    float average = 0.0;
-    for (int read = 0; read < nbOfReads; read++) {
-      int nextValue = weights[read][channel];
-      if ((read == 0) or (abs(nextValue - average) <= maxDeviation)) {
-        sum += nextValue;
-      } else {
-        sum += int(floor(average));
-      }
-      average = sum / (read + 1);
-    }
-    local->weights[channel] = int(floor(sum / nbOfReads));
-  }
+  delay(1000);
+  local->weights[0] = scale1.get_value(10);
+  Serial.print(local->weights[0]);
+  Serial.print("\t");
+  local->weights[1] = scale2.get_value(10);
+  Serial.print(local->weights[1]);
+  Serial.print("\t");
+  local->weights[2] = scale3.get_value(10);
+  Serial.print(local->weights[2]);
+  Serial.println();
 }
 
 void showLocalData(LocalData_t *local) {
@@ -292,12 +307,12 @@ void showLocalData(LocalData_t *local) {
   Serial.println(local->weights[1]);
   Serial.print("Scale3: ");
   Serial.println(local->weights[2]);
-  Serial.print("Scale4: ");
-  Serial.println(local->weights[3]);
-  Serial.print("Scale5: ");
-  Serial.println(local->weights[4]);
-  Serial.print("Scale6: ");
-  Serial.println(local->weights[5]);
+  //Serial.print("Scale4: ");
+  //Serial.println(local->weights[3]);
+  //Serial.print("Scale5: ");
+  //Serial.println(local->weights[4]);
+  //Serial.print("Scale6: ");
+  //Serial.println(local->weights[5]);
 }
 
 /******************************* Communication ********************************/
