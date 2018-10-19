afca60ddcb622bc0f4965e9b283f6e8a2cada561
robbiedeloose <robbiedeloose@users.noreply.github.com>
2018-10-19 08:33:14 +0100

    Revert "moved coordinator data to config file"
    This reverts commit fd9924fad8ffc8b9b994bbb7565033ec991770ad.

--- src/config.cpp
+++
@@ -1,7 +0,0 @@
-#include "config.h"
-
-void displayCoordinatorData(char* coordinatorAddressString) {
-    SerialMon.println(F("BeeNode v4.0.1a"));
-    SerialMon.print("Id: ");
-    SerialMon.println(coordinatorAddressString);
-} 
