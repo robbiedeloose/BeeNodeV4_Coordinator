6963f20761f9b74b094e6e5f4117e2b244f3479d
robbiedeloose <robbiedeloose@users.noreply.github.com>
2018-10-20 10:06:17 +0200

    moved gprs to external file

---
+++ src/gprs.cpp
@@ -0,0 +1,108 @@
+#include "gprs.h"
+
+  #define TINY_GSM_MODEM_SIM800
+  #include <TinyGsmClient.h>
+  #include <PubSubClient.h>
+  // Your GPRS credentials
+  const char apn[] = "hologram";
+  const char user[] = "";
+  const char pass[] = "";
+  // Server details
+  const char *broker = "m20.cloudmqtt.com";
+  const uint16_t mqttPort = 15913;
+  const char *mqttUser = "oseiokpx";
+  const char *mqttPswd = "31IMHCdWxVVt";
+
+  TinyGsm modem(SerialAT);
+  TinyGsmClient client(modem);
+  PubSubClient mqtt(client);
+  char mqttClient[17] = "";
+
+//////////// MQTT Code /////////////////////////////////////////////////////////
+void mqttInit(char* coordinatorAddressString) {
+  SerialMon.println(":: mqttInit");
+  mqtt.setServer(broker, mqttPort);
+  for(size_t i = 0; i < 17; i++) {
+    mqttClient[i]=coordinatorAddressString[i];
+  }
+  mqttClient[16] = '\0';
+  //mqtt.setCallback(mqttCallback); // we can use this to reply options to CO
+  SerialMon.print("Mqtt Client ID: ");
+  SerialMon.println(mqttClient);
+}
+
+void mqttRegister(char* coordinatorAddressString) {
+  SerialMon.println(":: mqttRegsister");
+  gprsConnectNetwork();
+  if (mqtt.connect(mqttClient, mqttUser, mqttPswd)) {
+    mqtt.publish("c/r", mqttClient);
+  }
+  mqtt.disconnect();
+  gprsEnd();
+  SerialMon.println("Registered CO");
+}
+
+void mqttSendData(LocalData_t *local) {
+  SerialMon.println(":: mqttSendData");
+  //gprsResetModem();
+  gprsConnectNetwork();
+  if (mqtt.connect(mqttClient, mqttUser, mqttPswd)) {
+    char buf[120] = "";
+    sprintf(buf, "%s,%i,%u,%d,%u", mqttClient, local->baseTemp, local->baseHum, local->baseLux, local->baseBat);
+    SerialMon.println(buf);
+    mqtt.publish("c/d", buf);
+    sprintf(buf, "%s,%i,%li,%li,%li,%li,%li,%li,%li,%li,%li", mqttClient, local->baseTemp, local->weights[0], local->weights[1], local->weights[2], local->weights[3], local->weights[4], local->weights[5], local->weights[6], local->weights[7], local->weights[8]);
+    SerialMon.println(buf);
+    mqtt.publish("c/s", buf);
+  }  
+  mqtt.disconnect();
+  gprsEnd();
+  delay(250); /// check if this can be removed
+}
+
+//////////// init gprs, connect and disconnect from network ////////////////////
+void gprsTest() {
+  gprsResetModem();
+  gprsConnectNetwork();
+  gprsEnd();
+}
+
+void gprsResetModem() {
+  SerialMon.println(":: gprsResetModem");
+  modem.restart();
+  String modemInfo = modem.getModemInfo();
+  SerialMon.print(F(" Modem: "));
+  SerialMon.println(modemInfo);
+}
+
+void gprsDisplayModemInfo() {
+  SerialMon.println(":: gprsResetModem");
+  //modem.restart();
+  String modemInfo = modem.getModemInfo();
+  SerialMon.print(F(" Modem: "));
+  SerialMon.println(modemInfo);
+}
+
+void gprsConnectNetwork() {
+  SerialMon.print(F(" Waiting for network..."));
+  if (!modem.waitForNetwork()) {
+    SerialMon.println(F(" fail"));
+    delay(10000);
+    return;
+  }
+  SerialMon.println(F(" OK"));
+  SerialMon.print(F(" Connecting to "));
+  SerialMon.print(apn);
+  if (!modem.gprsConnect(apn, user, pass)) {
+    SerialMon.println(F(" fail"));
+    delay(10000);
+    return;
+  }
+  SerialMon.println(F(" OK"));
+}
+
+void gprsEnd() {
+  modem.gprsDisconnect();
+  //gprsPowerOff();
+  SerialMon.println(" Disconnected");
+}
---
+++ src/gprs.h
@@ -0,0 +1,19 @@
+#ifndef GPRS_H   /* Include guard */
+#define GPRS_H
+
+#include <arduino.h>
+#include "pins.h"
+#include "config.h"
+
+  void gprsTest();
+  void gprsResetModem();
+  void gprsConnectNetwork();
+  void gprsEnd();
+  void gprsPowerOn();
+  void gprsPowerOff();  
+  // MQTT
+  void mqttInit(char* coordinatorAddressString);
+  void mqttRegister(char* coordinatorAddressString);
+  void mqttSendData(LocalData_t *local);
+
+#endif
