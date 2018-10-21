107089faa882bdb70f7d867a2b775d1ad62eea7c
Stef De Pooter <stefdepooter@gmail.com>
2018-10-21 11:44:37 +0200

    empty space + newline at end of files

--- src/config.cpp
+++ src/config.cpp
@@ -1,9 +1,9 @@
 #include "config.h"
 
 void displayCoordinatorData(char* coordinatorAddressString) {
-    SerialMon.println(F("BeeNode v4.0.1a"));
-    SerialMon.print("Id: ");
-    SerialMon.println(coordinatorAddressString);
+  SerialMon.println(F("BeeNode v4.0.1a"));
+  SerialMon.print("Id: ");
+  SerialMon.println(coordinatorAddressString);
 } 
 
 // display sleep interval
