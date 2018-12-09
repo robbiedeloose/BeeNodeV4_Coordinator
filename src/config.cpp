8da9f31ea1b6037756820ecab80d5e487f6043d3
robbiedeloose <robbiedeloose@users.noreply.github.com>
2018-12-09 20:33:12 +0100

    init

--- src/config.cpp
+++ src/config.cpp
@@ -6,6 +6,20 @@ void displayCoordinatorData(char* coordinatorAddressString) {
   SerialMon.println(coordinatorAddressString);
 } 
 
+void setPinModes() {
+  pinMode(A5,INPUT);
+  //led
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  //gprssleep
+  pinMode(GSM_RESET_PIN, OUTPUT);
+  digitalWrite(GSM_RESET_PIN, HIGH);
+  //sleep enable
+  pinMode(SLEEP_ENABLED, INPUT_PULLUP);
+  digitalWrite(SLEEP_ENABLED, HIGH);
+}
+
+
 // display sleep interval
 // display operator
 // display mqtt settings
